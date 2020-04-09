ad7a704388ca8004928b29ecd068e2fa6511630a
Ryan Letterio <rletterio42@gmail.com>
2020-04-09 00:22:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,5 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+def meal_choice(veg1, veg2, protein = "tofu")
+  puts "What a nutritious meal!"
+  puts "Hi, my name is #{name} and I ma learning to program in #{language}."
